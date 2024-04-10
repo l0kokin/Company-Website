f9159a77f8ae2c667b070fb6d38fb21534dd0b80
l0kokin <sali.gogishvili1@gmail.com>
2024-04-10 16:47:12 +0400

    I organized the styles, made arrows in the news section work using javascript

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ function toggleMenu() {
   var menu = document.getElementById("menu");
   menu.classList.toggle("show");
 }
+
+const cardsContainer = document.querySelector("#cards");
+const arrow1 = document.querySelector("#arrow1");
+const arrow2 = document.querySelector("#arrow2");
+const cardWidth = document.querySelector(".card--item").offsetWidth;
+let currentPosition = 0;
+
+arrow2.addEventListener("click", function () {
+  if (currentPosition < 0) {
+    currentPosition += cardWidth;
+    slideCards();
+  }
+});
+
+arrow1.addEventListener("click", function () {
+  const maxPosition = -((cardsContainer.childElementCount - 1) * cardWidth);
+  if (currentPosition > maxPosition) {
+    currentPosition -= cardWidth;
+    slideCards();
+  }
+});
+
+function slideCards() {
+  cardsContainer.style.transition = "transform 0.3s ease";
+  cardsContainer.style.transform = `translateX(${currentPosition}px)`;
+
+  setTimeout(() => {
+    cardsContainer.style.transition = "none";
+  }, 300);
+}
